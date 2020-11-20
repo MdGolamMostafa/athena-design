d4c1142612381e821c82f7d7322e35b5b0c2e49d
Nur A Alam <nuraalam376@gmail.com>
2020-11-20 07:59:40 +0600

    Added about section

--- src/Components/AllPages/AllPages.js
+++ src/Components/AllPages/AllPages.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import Footer from '../Footer/Footer';
-import GetSearchDesign from '../GetSearchDesign/GetSearchDesign';
-import Home from '../Home/Home';
+import React from "react";
+import About from "../About/About";
+import Footer from "../Footer/Footer";
+import GetSearchDesign from "../GetSearchDesign/GetSearchDesign";
+import Home from "../Home/Home";
 const AllPages = () => {
-    return (
-        <div>
-            <Home/>
-            <GetSearchDesign/>
-            <Footer/>
-        </div>
-    );
+  return (
+    <div>
+      <Home />
+      <About />
+      <GetSearchDesign />
+      <Footer />
+    </div>
+  );
 };
 
 export default AllPages;
--- src/Components/GetSearchDesign/GetSearchDesign.js
+++ src/Components/GetSearchDesign/GetSearchDesign.js
@@ -1,34 +1,37 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import SearchField from "react-search-field";
-import ReactSearchBox from 'react-search-box'
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import ReactSearchBox from "react-search-box";
 
 const GetSearchDesign = () => {
-    return (
-         <Container className = "mt-5 mb-5">
-             <Row>
-                 <Col xs={8} className="m-auto d-flex justify-content-center border border-primary" xl={6} md="8">
-                    <Row className="m-auto">
-                    {/* <h1>Bismillah</h1> */}
-                    <ReactSearchBox
-        placeholder="Enter your email address."
-        // value="Doe"
-        // data={this.data}
-        // callback={record => console.log(record)}
-      />
-                    </Row>
-                    {/* <Row>
+  return (
+    <Container className="mt-5 mb-5">
+      <Row>
+        <Col
+          xs={8}
+          className="m-auto d-flex justify-content-center border border-primary"
+          xl={6}
+          md="8"
+        >
+          <Row className="m-auto">
+            {/* <h1>Bismillah</h1> */}
+            <ReactSearchBox
+              placeholder="Enter your email address."
+              // value="Doe"
+              // data={this.data}
+              // callback={record => console.log(record)}
+            />
+          </Row>
+          {/* <Row>
                         <SearchField
                             placeholder="Search..."
                             searchText=" search "
                             classNames="test-class"
                         />
                     </Row> */}
-                   
-                 </Col>
-             </Row>
-         </Container>
-    );
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default GetSearchDesign;
