940662c79d530196ded2a06b1dd0938844c3d552
MdGolamMostafa <shakilhero11@gmail.com>
2020-11-20 08:26:02 +0600

    google-font and logo added

--- src/Components/AllPages/AllPages.js
+++ src/Components/AllPages/AllPages.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Footer from '../Footer/Footer';
-import GetSearchDesign from '../GetSearchDesign/GetSearchDesign';
 import Home from '../Home/Home';
 const AllPages = () => {
     return (
         <div>
-            <Home/>
-            <GetSearchDesign/>
-            <Footer/>
+            
+             <Home/>
+             <Footer/>
         </div>
     );
 };
