b461809e0a49f19e4758c6f955f5d5286bfcd4ec
MdGolamMostafa <shakilhero11@gmail.com>
2020-12-12 09:28:49 +0600

    about section updated

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,54 +5,54 @@ import "./About.css";
 const About = () => {
   return (
     <Container className="text-center" id="about">
-      <h1 className ="text-center">What we do</h1>
-      <h6 className ="text-center col-5 text-dark">
-        Our main focus is to make the User Experience very simple and easy.
-        Simplicity is our Strength
-      </h6>
+        <h1 className ="text-center">What we do</h1>
+            <h6 className = "text-dark">
+                Our main focus is to make the User Experience very <br/> simple and easy.
+                Simplicity is our Strength
+            </h6>
 
-      <CardDeck className="mt-5">
-        <Card >
-          <Card.Img className="mt-5" variant="top" src="/images/Group 65@2x.png" />
-          <Card.Body>
-            <Card.Title>Experience Design</Card.Title>
-            <Card.Text>
-              The point of using Lorem Ipsum is that it has a more-or-less
-              normal.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img className="mt-5" variant="top" src="/images/Group 66@2x.png" />
-          <Card.Body>
-            <Card.Title>Interface Design</Card.Title>
-            <Card.Text>
-              The point of using Lorem Ipsum is that it has a more-or-less
-              normal.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img className="mt-5" variant="top" src="/images/Group 69@2x.png" />
-          <Card.Body>
-            <Card.Title>Prototyping</Card.Title>
-            <Card.Text>
-              The point of using Lorem Ipsum is that it has a more-or-less
-              normal.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img className="mt-5"variant="top" src="/images/Group 72@2x.png" />
-          <Card.Body>
-            <Card.Title>Illustration</Card.Title>
-            <Card.Text>
-              The point of using Lorem Ipsum is that it has a more-or-less
-              normal.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </CardDeck>
+        <CardDeck className="mt-5">
+            <Card >
+                <Card.Img className="mt-5" variant="top" src="/images/Group 65@2x.png" />
+                    <Card.Body>
+                        <Card.Title>Experience Design</Card.Title>
+                            <Card.Text>
+                                The point of using Lorem Ipsum is that it has a more-or-less
+                                normal.
+                            </Card.Text>
+                    </Card.Body>
+            </Card>
+            <Card  className="card-interface">
+                <Card.Img className="mt-5" variant="top" src="/images/Group 66@2x.png" />
+                    <Card.Body >
+                          <Card.Title>Interface Design</Card.Title>
+                              <Card.Text>
+                                  The point of using Lorem Ipsum is that it has a more-or-less
+                                  normal.
+                              </Card.Text>
+                    </Card.Body>
+            </Card>
+            <Card>
+                <Card.Img className="mt-5" variant="top" src="/images/Group 69@2x.png" />
+                    <Card.Body>
+                        <Card.Title>Prototyping</Card.Title>
+                            <Card.Text>
+                                The point of using Lorem Ipsum is that it has a more-or-less
+                                normal.
+                            </Card.Text>
+                    </Card.Body>
+            </Card>
+            <Card>
+                <Card.Img className="mt-5"variant="top" src="/images/Group 72@2x.png" />
+                    <Card.Body>
+                        <Card.Title>Illustration</Card.Title>
+                            <Card.Text>
+                                The point of using Lorem Ipsum is that it has a more-or-less
+                                normal.
+                            </Card.Text>
+                    </Card.Body>
+            </Card>
+        </CardDeck>
     </Container>
   );
 };
